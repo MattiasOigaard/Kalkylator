574dce34f40627692c9b29f1f71ea389e387700a
Mattias Öigaard <mattias.oigaard@gmail.com>
2019-10-25 12:13:03 +0200

    Kalkylator utan loop

--- Kalkylator/Program.cs
+++ Kalkylator/Program.cs
@@ -7,15 +7,36 @@ namespace Kalkylator
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the first number: ");
-            int numberOne = Convert.ToInt32(Console.ReadLine());
+            double numberOne = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the second number: ");
-            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            double numberTwo = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Press 1 for addition:");
             Console.WriteLine("Press 2 for subtraction");
-            Console.WriteLine("Press 3 for multification");
-            Console.WriteLine("Press 4 for divition");
-            int numberThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Press 3 for multiplication");
+            Console.WriteLine("Press 4 for division");
+            double numberThree = Convert.ToDouble(Console.ReadLine());
+            switch (numberThree)
+            {
+                case 1:
+                    Console.WriteLine(numberOne + numberTwo);
+                    break;
+                case 2:
+                    Console.WriteLine(numberOne - numberTwo);
+                    break;
+                case 3:
+                    Console.WriteLine(numberOne * numberTwo);
+                    break;
+                case 4:
+                    if (numberTwo != 0)
+                        Console.WriteLine(numberOne / numberTwo);
+                    else
+                        Console.WriteLine("Cant divide by zero");
+                    break;
+                    
+
+
+            }
         }
     }
 }
